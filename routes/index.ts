458d5085df8558538de6c22cb7e5952ccaec51bb
kitchanismo <cristian.betsayda@gmail.com>
2024-11-04 18:55:16 +0800

    add comments

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,6 @@
 //clean
 import { extract } from 'express-extract-routes'
+//used relative path to get the controller during runtime
 import { UserController } from '../controllers/userController'
 import { ServerController } from '../controllers/serverController'
 
